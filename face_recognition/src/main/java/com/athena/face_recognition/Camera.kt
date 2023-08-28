207db8a6260523084efe2773fad269588a769c16
kof99athena <kof99athena@gmail.com>
2023-08-29 00:28:46 +0900

    Camera 퍼미션 받기

--- face_recognition/src/main/java/com/athena/face_recognition/Camera.kt
+++ face_recognition/src/main/java/com/athena/face_recognition/Camera.kt
@@ -1,20 +1,30 @@
 package com.athena.face_recognition
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.athena.cameratest.Manifest
 import com.google.common.util.concurrent.ListenableFuture
 import java.lang.Exception
 import java.util.concurrent.Executors
 /* 카메라 프리뷰를 세팅할 수 있는 class */
 
+//25. 카메라 권한이 없다가 권한이 다시 생긴 경우! : ActivityCompat.OnRequestPermissionsResultCallback
 
-class Camera(private val context:Context) {
+class Camera(private val context:Context) : ActivityCompat.OnRequestPermissionsResultCallback {
+
+
+
     //11. Camera class 생성. context 생성자
 
     //12. 프리뷰 늦은 초기화하기
@@ -46,6 +56,21 @@ class Camera(private val context:Context) {
     fun initCamera(layout: ViewGroup){
         previewView = PreviewView(context)
         layout.addView(previewView)
+
+        //24
+        permissonCheck(context)
+    }
+
+
+    //23. PermissonUtil을 가지고 퍼미션 체크하기
+    private fun permissonCheck(context: Context){
+        val permissionList = listOf(Manifest.permission.CAMERA)
+        if (!PermissionUtil.checkPermission(context,permissionList)){
+            //퍼미션 안받아오면 PermissionUtil이 리퀘스트하게 해줭
+            PermissionUtil.requestPermission(context as Activity, permissionList)
+        } else {
+            openPreview()
+        }
     }
 
     //17. 프리뷰가 바로 활성화 될수있게끔
@@ -69,7 +94,33 @@ class Camera(private val context:Context) {
         }catch(e:Exception) {
             e.stackTrace
         }
+    }//startPreview method
+
+    //26. override해서 함수를 얻어오자. 권한을 획득했을때
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        var flag = true
+        if (grantResults.isNotEmpty()){
+            for ((i,_)in permissions.withIndex()){
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                    flag = false
+                }
+            }
+            if (flag){
+                //만약 true면 프리뷰를 열고
+                openPreview()
+            }else{
+                //아니면 토스트 띄워
+                Toast.makeText(context, "권한이 없습니다. ", Toast.LENGTH_SHORT).show()
+                (Context as Activity).finish()
+            }
+        }
     }
+
+
 }
 
 
---
+++ face_recognition/src/main/java/com/athena/face_recognition/PermissionUtil.kt
@@ -0,0 +1,34 @@
+package com.athena.face_recognition
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+//20. 퍼미션을 받는 object 만들기 (체크와 리퀘스트 요구하는거)
+object PermissionUtil {
+
+
+    //21. 퍼미션 받았는지 안받았는지 확인
+    fun checkPermission(context : Context, permissionList : List<String>) : Boolean {
+        permissionList.forEach {
+            //퍼미션 받았는지 안받았는지 확인
+            if(ContextCompat.checkSelfPermission(
+                context,
+                it //너가 받은 it 즉 퍼미션
+            )== PackageManager.PERMISSION_DENIED){
+                //퍼미션 안받았으면 false
+                return false
+            }
+        } //forEach문
+
+        return true //퍼미션 받았으면 true
+
+    } //checkPermission method
+
+    //22. 체크 완료 했으면 퍼미션 요구해야징
+    fun requestPermission(activity:Activity, permissionList: List<String>){
+        ActivityCompat.requestPermissions(activity,permissionList.toTypedArray(),1)
+        //request 코드 일단 아무거나 넣기, 나중에는 사용처에 맞게 넣어야함..
+    }
+}
